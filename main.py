3d0c7c03c3164501886c679c7bed9d0fe731e038
Mathman <70949660+17e23052@users.noreply.github.com>
2021-09-13 11:01:05 +0000

    A1 code with inputs

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-game = {"character": "Harry" ,
-       "setting": "Neverland",
-       "task": "Run"}
- 
-print("Character:", game["character"])
-print("Setting:", game["setting"])
-print("Task:", game["task"])
+game = {"character": input("Enter character: ") ,
+       "setting": input("Enter setting: "),
+       "task": input("Enter task: ")}
+
 print() 
- 
-game["character"] = "Lola"
-game["setting"] = "The City"
-game["task"] = "Find the house"
- 
 print("Character:", game["character"])
 print("Setting:", game["setting"])
 print("Task:", game["task"])
