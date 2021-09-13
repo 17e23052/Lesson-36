e03dfd300caa686fff46f7bf47c528c91514600c
Mathman <70949660+17e23052@users.noreply.github.com>
2021-09-13 10:58:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+game = {"character": "Harry" ,
+       "setting": "Neverland",
+       "task": "Run"}
+ 
+print("Character:", game["character"])
+print("Setting:", game["setting"])
+print("Task:", game["task"])
+print()
+ 
+game["character"] = "Lola"
+game["setting"] = "The City"
+game["task"] = "Find the house"
+ 
+print("Character:", game["character"])
+print("Setting:", game["setting"])
+print("Task:", game["task"])
