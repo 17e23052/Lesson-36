b634d5d3911267484fbc8328a500cb7ad23ba4cf
Mathman <70949660+17e23052@users.noreply.github.com>
2021-09-14 13:26:58 +0000

    A2 added highscore

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-game = {"character": input("Enter character: ") ,
-       "setting": input("Enter setting: "),
-       "task": input("Enter task: ")}
+players = []
 
-print() 
-print("Character:", game["character"])
-print("Setting:", game["setting"])
-print("Task:", game["task"])
+add_players = True
+while add_players:
+    print("Enter a username:")
+    username = input()
+    print("Enter a password:")
+    password = input()
+    print("Enter a score")
+    score = input()
+    print("Enter your highest score:")
+    highscore = input()
+
+    player = {"username" : username,
+              "password" : password,
+              "score" : score,
+              "highscore": highscore}
+
+    players.append(player)
+
+    print("Would you like to add another player? Y/N")
+    answer = input().upper()
+    if answer == "N":
+        add_players = False
+
+print(players)
